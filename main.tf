4ff5ef22302637b717ed9d528106eebb3ea6222d
sreesai169 <sreesai169@gmail.com>
2023-07-19 16:30:07 +0530

    Committing terraform changes for creating instances

--- main.tf
+++ main.tf
@@ -50,9 +50,9 @@ resource "aws_autoscaling_group" "asg" {
   dynamic "tag" {
     for_each = local.asg_tags
     content {
-      key =
+      key = tag.key
       propagate_at_launch = true
-      value = tag.value.value
+      value = tag.value
     }
   }
 }
